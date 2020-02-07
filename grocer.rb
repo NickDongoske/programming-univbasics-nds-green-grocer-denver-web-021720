b9c9afb66b593bbf909177679fc3012fc1020ca2
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-07 21:13:38 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ def apply_coupons(cart, coupons)
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item && cart_item[:count] >= coupons[i][:num]
       if cart_item_with_coupon
-        cart_item_with_coupon[:count]
+        cart_item_with_coupon[:count] += coupons[i][:num]
     end
   i += 1
 end
