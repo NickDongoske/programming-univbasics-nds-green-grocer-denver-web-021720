16075226fc75dd9f53afc814bbf4a046cd8af4e0
Nicholas Dongoske <nickdongoske@gmail.com>
2020-02-07 21:23:44 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -73,6 +73,8 @@ def apply_clearance(cart)
   while i < cart.length do
     if cart[i][:clearance]
       cart[i][:price] = (cart[i][:price]) - (cart[i][:price] * 0.20)).round(2)
+    end
+  end
 end
 
 def checkout(cart, coupons)
